88108513d1cd5618ae194dbcfed0ae270617fa09
Matthieu <matmarti@mathicsepc54.epfl.ch>
2018-05-14 17:32:19 +0200

    sum to mean

--- criterions.py
+++ criterions.py
@@ -12,7 +12,7 @@ class Loss(object):
 
 class LossMSE(object):
     def function(self,output,expected):
-        return torch.sum(torch.pow(expected - output,2))
+        return torch.mean(torch.pow(expected - output,2))
 
     def grad(self,output,expected):
         return -2 * (expected - output)
